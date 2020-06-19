0b0b5ede7d744eac592bbd33b536a5e206fc7267
Nathannael <nathannael2009@gmail.com>
2020-06-19 20:01:51 -0300

    Remove unused tokenizer from word_counter

--- stream_listeners/word_counter.py
+++ stream_listeners/word_counter.py
@@ -1,12 +1,12 @@
+from lib.tools import save_to_file
+import re
+
 import tweepy
 import nltk
 import spacy
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
-from lib.tools import save_to_file
 
-tokenizer = RegexpTokenizer(r'\w+')
-nltk.download('stopwords')
 nlp = spacy.load("en_core_web_md")
 
 words = []
@@ -14,21 +14,10 @@ result = {}
 
 class MyStreamWordCounter(tweepy.StreamListener):
   def on_status(self, status):
-    result = self.tokenize_using_spacy(status.text, 'NOUN')
+    tweet_without_symbols = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",status.text).split())
+
+    result = self.tokenize_using_spacy(tweet_without_symbols, 'NOUN')
     save_to_file(result)
-
-  def tokenize_by_stopwords(self, tweet):
-    banlist = stopwords.words('english')
-    banlist.extend(['rt', 'https', 't', 'co', 'q'])
-
-    tweet_text = tweet.lower()
-
-    tokens = [ word for word in tokenizer.tokenize(tweet_text) if not word in banlist ]
-
-    words.extend(tokens)
-
-    fdist = nltk.FreqDist(words)
-    return dict(fdist.most_common(10))
 
   def tokenize_using_spacy(self, tweet, pos):
     doc = nlp(tweet)
