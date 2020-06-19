a283767bab0079d3218b46131c36d9e11b92d813
Nathannael <nathannael2009@gmail.com>
2020-06-19 18:47:17 -0300

    Allow dashboard to control the analysis

--- analysis.py
+++ analysis.py
@@ -3,24 +3,32 @@ load_dotenv()
 
 import os
 import tweepy
+import time
 
 # from stream_listeners.sentiment_counter import MyStreamSentimentCounter
 # from stream_listeners.spacy_sentiment_counter import MySpacySentimentCounter
 # from stream_listeners.word_counter import MySpacySentimentCounter
 from stream_listeners.word_counter import MyStreamWordCounter
 from stream_listeners.series_ranking import MyStreamSeriesRanking
-from dashboards.plot_graphs import PlotGraphs
+
 from lib.tools import SERIES
+from lib.tools import save_to_file
 
 
-auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_secret'))
-auth.set_access_token(os.getenv('access_token'), os.getenv('access_token_secret'))
+class Analysis:
+  @classmethod
+  def perform(self, keyword):
+    auth = tweepy.OAuthHandler(os.getenv('consumer_key'), os.getenv('consumer_secret'))
+    auth.set_access_token(os.getenv('access_token'), os.getenv('access_token_secret'))
 
-api = tweepy.API(auth)
-myStreamListener = MyStreamWordCounter()
+    api = tweepy.API(auth)
+    myStreamListener = MyStreamWordCounter()
 
-myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-
-print("iniciando")
-myStream.filter(track='blacklivesmatter', languages=['en'], is_async=True)
-PlotGraphs.commence_the_plotting()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+    myStream.words = []
+    myStream.result = {}
+    print('Iniciando')
+    print(keyword)
+    save_to_file({})
+    myStream.filter(track=[keyword], languages=['en'], is_async=True)
+    return myStream
---
+++ plot_graphs.py
@@ -0,0 +1,62 @@
+import datetime
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+from dash.dependencies import Input, Output
+import json
+from analysis import Analysis
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+streamer = None
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    [
+      html.H1(children='Tweet-Analysis'),
+
+      html.Div(dcc.Input(id='input-on-submit', type='text')),
+      html.Button('Submit', id='submit-val', n_clicks=0),
+      html.Div(id='container-button-basic',
+              children='Enter a value and press submit'),
+
+      dcc.Graph(id='live-graph', animate=True),
+      dcc.Interval(
+        id='graph-update',
+        interval=1*1000, # in milliseconds
+        n_intervals=0
+      ),
+    ]
+)
+
+@app.callback(
+  dash.dependencies.Output('container-button-basic', 'children'),
+  [dash.dependencies.Input('submit-val', 'n_clicks')],
+  [dash.dependencies.State('input-on-submit', 'value')]
+)
+def update_output_div(n_clicks, input_value):
+  global streamer
+
+  if streamer is not None:
+    streamer.disconnect()
+  streamer = Analysis.perform(input_value)
+  return 'Iniciando'
+
+@app.callback(
+  Output('live-graph', 'figure'),
+  [Input('graph-update', 'n_intervals')]
+)
+def update_graph_tweets(n):
+  data = json.load(open("data.json"))
+  data = {k: v for k, v in sorted(data.items(), key=lambda item: item[1], reverse=True)}
+
+  data =  {'x': list(data.keys())[:15], 'y': list(data.values())[:15], 'type': 'bar', 'name': 'SF'}
+
+  return {'data': [data] }
+
+if __name__ == '__main__':
+  app.run_server(debug=True)
+
